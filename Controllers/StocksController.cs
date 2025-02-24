1953146b88e95bdb0a1799848b64db913ef94a4f
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-02-24 22:47:00 +0100

    [UPD] added DELETE on StockController.cs

--- Controllers/StocksController.cs
+++ Controllers/StocksController.cs
@@ -84,23 +84,19 @@ namespace dotnet_api_test.Controllers
 
         // DELETE: api/Stocks/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteStock(int id)
+        public async Task<IActionResult> Delete([FromRoute] int id)
         {
-            var stock = await _context.Stocks.FindAsync(id);
-            if (stock == null)
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (stockModel == null)
             {
                 return NotFound();
             }
 
-            _context.Stocks.Remove(stock);
+            _context.Stocks.Remove(stockModel);
             await _context.SaveChangesAsync();
 
             return NoContent();
         }
 
-        private bool StockExists(int id)
-        {
-            return _context.Stocks.Any(e => e.Id == id);
-        }
     }
 }
