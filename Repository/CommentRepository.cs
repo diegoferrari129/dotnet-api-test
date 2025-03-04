a7c4fb3433e07b5e0c372651082755c86df7805f
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-03-04 22:43:55 +0100

    [UPD] added repository and interface for Comment

---
+++ Repository/CommentRepository.cs
@@ -0,0 +1,17 @@
+﻿using dotnet_api_test.interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_api_test;
+
+public class CommentRepository : ICommentkRepository
+{
+    private readonly ApplicationDbContext _context;
+    public CommentRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<List<Comment>> GetAllAsync()
+    {
+        return await _context.Comments.ToListAsync();
+    }
+}
